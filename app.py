7f2c082a5a448e7efc24f627caab2dce3c71aea6
Matt <matgonzalez@hotmail.fr>
2022-08-24 22:26:50 +0200

    create application class to launch app

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 """The entry point was fixed with the correct class names"""
+import tkinter as tk
+from controllers.domain_controller import Controller
+from models.model import Model
+from views.frame import View
+from services.your_service import DomaineService
 
-from controllers.interface_controller import AbstractController
+
+class Application(tk.Tk):
+    def __init(self):
+        self.master = super().__init__()
+
+    def attach(self, controller, view, service):
+        view = view(self.master)
+        controller.bind(view, service)
 
 
-if __name__ == '__main__':
-    controller = AbstractController()
-    controller.main()
+if __name__ == "__main__":
+    view = View
+    model = Model
+    service = DomaineService(model)
+    controller = Controller()
+    app = Application()
+    app.attach(view=view, service=service, controller=controller)
+    app.mainloop()
